9cdd9a0beb859c4142038c19a3b3d40cb40ab7a9
Aliou Diallo <aliou.diallo@me.com>
2012-06-02 15:55:19 +0200

    -

--- 001.c
+++ 001.c
@@ -5,10 +5,10 @@
 #include <stdio.h>
 int main(int argc, const char * argv[]) { 
   int sum = 0;
-  for (int i = 0; i < 1000 ; i++) {
-    if (i % 3 == 0 || i% 5 == 0)
-      sum += i;
-  }
+
+  for (int i = 0; i < 1000 ; i++) 
+    sum += (i % 3 == 0 || i % 5 == 0) ? i : 0;
+
   printf("sum = %d\n", sum);
   return 0;
 }
